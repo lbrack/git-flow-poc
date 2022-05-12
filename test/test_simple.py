8cbc9e67ff8b677c6cc6ce70779310efd21d9816
Laurent Brack <laurent.brack@gmail.com>
2022-05-12 13:52:54 -0700

    4.5

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,3 +1,5 @@
 import os
 def test_a():
-    assert str(os.environ['GITHUB_REF_NAME']) == "staging-4.5"
+    expected = "staging-4.6"
+    print(f"{os.environ['GITHUB_REF_NAME']} expected {expected}")
+    assert str(os.environ['GITHUB_REF_NAME']) == expected
